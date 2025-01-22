2a5d6c53d9e9a27cf1a5b19dd888be5d7a886b23
Pilou-face <yevgueniduverger@gmail.com>
2025-01-22 17:44:42 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,16 @@
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
+from flask import render_template
+from flask import json
+from urllib.request import urlopen
+from werkzeug.utils import secure_filename
+import sqlite3
 
+app = Flask(__name__)                                                                                                                  
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+                                                                                                                                    
+if __name__ == "__main__":
+  app.run(debug=True)
