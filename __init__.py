7a0edf5e565173f8e0abd8e61aeb24bd1b582462
Pilou-face <yevgueniduverger@gmail.com>
2025-01-22 17:49:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,9 +7,9 @@ import sqlite3
 
 app = Flask(__name__)                                                                                                                  
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
 
                                                                                                                                     
 if __name__ == "__main__":
