4dfc194e844c9ca77d296df8ac39b2c06017fe45
Pilou-face <yevgueniduverger@gmail.com>
2025-01-23 10:37:16 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -7,4 +7,4 @@ class Livre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     titre = db.Column(db.String(150), nullable=False)
     auteur = db.Column(db.String(150), nullable=False)
-    annee_publication = db.Column(db.Integer, nullable=False
+    annee_publication = db.Column(db.Integer, nullable=False)
