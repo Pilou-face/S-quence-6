7b5a1b694f1dca266fa4fff34f637da70c3ee767
Pilou-face <yevgueniduverger@gmail.com>
2025-01-23 09:20:14 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,15 +11,15 @@ app = Flask(__name__)
 def page1():
     return render_template('page1.html')
 
-@app.route('/page1')
+@app.route('/page2')
 def page2():
     return render_template('page2.html')
 
-@app.route('/page1')
+@app.route('/page3')
 def page3():
     return render_template('page3.html')
 
-@app.route('/page1')
+@app.route('/page4')
 def page4():
     return render_template('page4.html')
 
