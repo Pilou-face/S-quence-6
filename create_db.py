26c98283bbe1642215c65dd1cd4403ecf172ee1a
Pilou-face <yevgueniduverger@gmail.com>
2025-01-23 10:06:45 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,1 +1,10 @@
 
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Livre(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titre = db.Column(db.String(150), nullable=False)
+    auteur = db.Column(db.String(150), nullable=False)
+    annee_publication = db.Column(db.Integer, nullable=False
