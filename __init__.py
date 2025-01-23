cfb03d955070bd68a52bf0c9f37a957e528c0241
Pilou-face <yevgueniduverger@gmail.com>
2025-01-23 09:26:33 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,3 +27,6 @@ def page4():
 def HelloWord():
     return render_template('index.html')
 
+                                                                                                                                    
+if __name__ == "__main__":
+  app.run(debug=True)
