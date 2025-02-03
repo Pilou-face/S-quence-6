c9dc9cff9321218defd0b0db8cbcfae3549f52af
Pilou-face <yevgueniduverger@gmail.com>
2025-02-03 16:32:05 +0100

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,10 +1,20 @@
+import sqlite3
 
-from flask_sqlalchemy import SQLAlchemy
+connection = sqlite3.connect('database.db')
 
-db = SQLAlchemy()
+with open('schema.sql') as f:
+    connection.executescript(f.read())
 
-class Livre(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    titre = db.Column(db.String(150), nullable=False)
-    auteur = db.Column(db.String(150), nullable=False)
-    annee_publication = db.Column(db.Integer, nullable=False)
+cur = connection.cursor()
+
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUPONT', 'Emilie', '123, Rue des Lilas, 75001 Paris'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEROUX', 'Lucas', '456, Avenue du Soleil, 31000 Toulouse'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('MARTIN', 'Amandine', '789, Rue des Érables, 69002 Lyon'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('TREMBLAY', 'Antoine', '1010, Boulevard de la Mer, 13008 Marseille'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LAMBERT', 'Sarah', '222, Avenue de la Liberté, 59000 Lille'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('GAGNON', 'Nicolas', '456, Boulevard des Cerisiers, 69003 Lyon'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('DUBOIS', 'Charlotte', '789, Rue des Roses, 13005 Marseille'))
+cur.execute("INSERT INTO clients (nom, prenom, adresse) VALUES (?, ?, ?)",('LEFEVRE', 'Thomas', '333, Rue de la Paix, 75002 Paris'))
+
+connection.commit()
+connection.close()
